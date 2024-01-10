0618df2dd4a0210a3c97337a62bc282af55c509c
Bilal El-aney <bilalaney084@gmail.com>
2024-01-10 15:30:43 +0300

    For this commit, I have deleted the defaults and built the first version of this menu.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="main--container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza(props) {
+  const styles = props.sold
+    ? {
+        // If the pizza was sold out, then we will add a grey filter to the whole
+        // component
+        filter: "grayscale(100%)",
+      }
+    : {};
+
+  return (
+    <div className="pizza" style={styles}>
+      <img src={props.photo} alt="Pizza" />
+      <div className="pizza--info">
+        <h2>{props.name}</h2>
+        <h3>{(props.sold && "SOLD OUT") || `${props.price}$`}</h3>
+        <p>{props.ingredients}</p>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return <h1 className="header">Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  const pizzaElements = pizzaData.map((ele) => (
+    <Pizza
+      photo={ele.photoName}
+      price={ele.price}
+      name={ele.name}
+      ingredients={ele.ingredients}
+      sold={ele.soldOut}
+    />
+  ));
+
+  return <div className="menu-container">{pizzaElements}</div>;
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>We're open till 22:00, visit as ASAP...</p>
+      <button className="order--button">ORDER NOW!</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
